29b2bfb1f6acf75825c0be9749eefdbe6f55a2ad
miecz <maciej.szczesnik@autograf.pl>
2017-05-14 12:56:55 +0200

    3

---
+++ 2015Zaoczne/Assets/II ROK/Animacje - poruszanie/Lookat/TurretLookat.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretLookat : MonoBehaviour {
+
+    public Transform rotor;
+    public Transform gun;
+    public Transform target;
+
+    public float rotationSpeed = 0.1f;
+
+    Vector3 toTargetRaw;
+    Vector3 toTarget;
+
+    Vector3 gunRot;
+    Vector3 rotorRot;
+
+    private void Start()
+    {
+        toTarget = gun.forward;
+    }
+
+    void LookAt()
+    {
+        toTargetRaw = target.position - gun.position;
+        toTarget = Vector3.Slerp(toTarget, toTargetRaw, Time.deltaTime * rotationSpeed);
+
+        rotorRot = toTarget;
+        rotorRot.y = 0f;
+        gunRot = toTarget;
+
+        rotor.rotation = Quaternion.LookRotation(rotorRot);
+        gun.rotation = Quaternion.LookRotation(gunRot);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        LookAt();
+	}
+}
---
+++ 2015Zaoczne/Assets/II ROK/Player/LayerWeight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerWeight : MonoBehaviour {
+    Animator anim;
+    [Range(0f,1f)]
+    public float animWeight;
+
+    float targetWeight;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+    
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            targetWeight = 1f;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            targetWeight = 0f;
+        }
+        animWeight = Mathf.Lerp(animWeight, targetWeight, Time.deltaTime*2f);
+        anim.SetLayerWeight(1, animWeight);
+		
+	}
+}
